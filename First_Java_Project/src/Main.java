7e1e0406c54edc29a1d1577e289874de7d0e8673
Gbenga48 <gneskyyleeh48@gmail.com>
2022-12-05 17:46:07 +0100

    added new files

---
+++ First_Java_Project/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("this is my first java work!");
+        System.out.println("My name is Gbenga Moses");
+        System.out.println("Im a software quality assurance engineer");
+        System.out.println("i want to be the best software tester ever");
+    }
+}
